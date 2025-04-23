38f2d8ce6fd2364f5074a547f00ef5d8184331c1
noland <bluestar.tnt01@gmail.com>
2025-04-23 09:10:04 -0400

    add another update

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by <bluestar.tbt01@gmail.com>
+ # Here is a new update by <bluestar.tnt01@gmail.com>
+# Here is another update by <your GitHub username><bluestar.tnt01@gmail.com>
